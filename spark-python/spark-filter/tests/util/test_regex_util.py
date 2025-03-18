9b8a2d0ecd9ccaef1423827218ea92920749a40f
Mohammad Mahfooz Alam <mohammadmahfooz.alam@gainwelltechnologies.com>
2025-03-18 13:29:40 +0000

    fixed selected column filter

--- spark-python/spark-filter/tests/util/test_regex_util.py
+++ spark-python/spark-filter/tests/util/test_regex_util.py
@@ -1,7 +1,28 @@
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, lit, avg, stddev
 
-from util.regex_util import replace_filter_query_expression
+from util.regex_util import replace_filter_query_expression, select_filter_columns
 import pytest
+import sys
+
+
+def test_expect_column_pair_values_to_be_equal(spark):
+    key = "expect_column_values_to_not_match_regex_list"
+    param = {
+        "regex_list": ["^\\d{3}/\\d{2}$", "^\\d{1,3}/\\d{1,3}$"],
+        "column": "Blood_Pressure",
+        "result_format": {
+            "return_unexpected_index_query": True,
+            "result_format": "COMPLETE",
+            "partial_unexpected_count": 20,
+            "include_unexpected_rows": False,
+        },
+    }
+
+    filter_query = replace_filter_query_expression(key, param)
+    expected_outcome = col("Blood_Pressure").rlike("^\\d{3}/\\d{2}$|^\\d{1,3}/\\d{1,3}$")
+    comparision_result = str(filter_query) == str(expected_outcome)
+
+    assert comparision_result
 
 
 def test_expect_column_values_to_not_match_regex_list(spark):
@@ -44,6 +65,32 @@ def test_expect_column_values_to_match_regex(spark):
     assert comparision_result
 
 
+def test_expect_column_value_z_scores_to_be_between(spark):
+    key = "expect_column_value_z_scores_to_be_between"
+    param = {
+        "column": "transform_cd",
+        "threshold": 0,
+        "double_sided": True,
+        "result_format": {
+            "return_unexpected_index_query": True,
+            "result_format": "COMPLETE",
+            "partial_unexpected_count": 20,
+            "include_unexpected_rows": False,
+        },
+    }
+
+    filter_query = replace_filter_query_expression(key, param)
+    expected_outcome = (((col("transform_cd") - avg("transform_cd")) / stddev("transform_cd"))) >= lit(0)
+    print(f"filter_query: {filter_query}")
+    print(f"expected_outcome: {expected_outcome}")
+    comparision_result = str(filter_query) == str(expected_outcome)
+
+    selected_column = select_filter_columns(param)
+    assert selected_column == "transform_cd"
+
+    assert comparision_result
+
+
 def test_expect_multicolumn_sum_to_equal(spark):
     key = "expect_multicolumn_sum_to_equal"
     param = {
@@ -58,8 +105,14 @@ def test_expect_multicolumn_sum_to_equal(spark):
         },
     }
     filter_query = replace_filter_query_expression(key, param)
-    expected_outcome = col("Blood_Pressure").rlike("^I.*$")
+    expected_outcome = ((col("load_cd") + lit(0)) + col("transform_cd")) >= lit(0)
+    print(f"filter_query: {filter_query}")
+    print(f"expected_outcome: {expected_outcome}")
     comparision_result = str(filter_query) == str(expected_outcome)
+
+    selected_column = select_filter_columns(param)
+    assert selected_column == "load_cd,transform_cd"
+
     assert comparision_result
 
 
@@ -79,6 +132,10 @@ def test_expect_column_values_to_not_match_regex(spark):
     filter_query = replace_filter_query_expression(key, param)
     expected_outcome = col("Blood_Pressure").rlike("^I.*$")
     comparision_result = str(filter_query) == str(expected_outcome)
+
+    selected_column = select_filter_columns(param)
+    assert selected_column == "Blood_Pressure"
+
     assert comparision_result
 
 
@@ -99,4 +156,10 @@ def test_expect_column_values_to_match_regex_list(spark):
     expected_outcome = ~col("Blood_Pressure").rlike("^\\d{3}/\\d{2}$|^\\d{1,3}/\\d{1,3}$")
     comparision_result = str(filter_query) == str(expected_outcome)
 
+    selected_column = select_filter_columns(param)
+    assert selected_column == "Blood_Pressure"
     assert comparision_result
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-qq", "-vv"]))
