cd569659b79cafe4c61b0a890e2af9f1ef564391
Mohammad Mahfooz Alam <mohammadmahfooz.alam@gainwelltechnologies.com>
2025-05-15 12:25:09 +0000

    pyspark ds

--- spark-python/pyspark-datasource/pyspark-ds-api/src/authentication/api_key/query/api_key_query.py
+++ spark-python/pyspark-datasource/pyspark-ds-api/src/authentication/api_key/query/api_key_query.py
@@ -1,11 +1,32 @@
 import os
 import sys
 from pathlib import Path
-
-import yaml
 from pyspark.sql import SparkSession
+from util.config_loader import load_config
+from util.data_processor import read_api
 
-from authentication.etl_extract import read_api
+# from authentication.etl_extract import read_api
+
+# os.environ["JAVA_HOME"] = os.environ["JAVA_HOME_11"]
+# os.environ["PYSPARK_PYTHON"] = sys.executable
+
+
+# def main():
+#     spark = SparkSession.builder.appName("REST_API_Ingestion").getOrCreate()
+#     config_path = Path(__file__).parents[0] / "api_key_query.yaml"
+#     print(f"Loading config from {config_path}")
+#     with open(file=config_path, mode="r", encoding="utf-8") as f:
+#         config = yaml.safe_load(f)
+#     print(f"Config loaded: {config}")
+#     print("Extracting data from API...")
+#     df = read_api(spark, config)
+#     print(f"Data extracted: {df.show(truncate=False, n=100)}")
+#     print("Data extraction complete.")
+
+
+# if __name__ == "__main__":
+#     main()
+
 
 os.environ["JAVA_HOME"] = os.environ["JAVA_HOME_11"]
 os.environ["PYSPARK_PYTHON"] = sys.executable
@@ -14,14 +35,9 @@ os.environ["PYSPARK_PYTHON"] = sys.executable
 def main():
     spark = SparkSession.builder.appName("REST_API_Ingestion").getOrCreate()
     config_path = Path(__file__).parents[0] / "api_key_query.yaml"
-    print(f"Loading config from {config_path}")
-    with open(file=config_path, mode="r", encoding="utf-8") as f:
-        config = yaml.safe_load(f)
-    print(f"Config loaded: {config}")
-    print("Extracting data from API...")
+    config = load_config(config_path)
     df = read_api(spark, config)
-    print(f"Data extracted: {df.show(truncate=False, n=100)}")
-    print("Data extraction complete.")
+    df.show(truncate=False, n=100)
 
 
 if __name__ == "__main__":
---
+++ spark-python/pyspark-datasource/pyspark-ds-api/src/authentication/auth_util.py
@@ -0,0 +1,61 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def get_auth_headers(auth):
+    if not auth or auth.get("type") == "none":
+        return {}, None
+
+    if auth["type"] == "basic":
+        print("Basic Authentication")
+        return {}, HTTPBasicAuth(auth["username"], auth["password"])
+
+    if auth["type"] == "bearer":
+        print("Bearer Authentication")
+        return {"Authorization": f"Bearer {auth['token']}"}, None
+
+    if auth["type"] == "apikey":
+        print("API Key Authentication")
+        if auth["in"] == "header":
+            print("API Key in Header")
+            return {auth["name"]: auth["value"]}, None
+        # if in query, return empty and let caller add to params
+        return {}, None
+
+    if auth["type"] == "oauth2_client_credentials":
+        print("OAuth2 Client Credentials flow")
+        headers = auth.get("headers", {})
+        print("Headers: ", headers)
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": auth["clientId"],
+            "client_secret": auth["clientSecret"],
+            "scope": auth.get("scope", ""),
+        }
+        print("Data: ", data)
+        token_resp = requests.post(url=auth["tokenUrl"], headers=headers, data=data, timeout=60)
+        token_resp.raise_for_status()
+        token = token_resp.json()["access_token"]
+        return {"Authorization": f"Bearer {token}"}, None
+
+    if auth["type"] == "oauth2_form_client_credentials":
+        print("OAuth2 Client Credentials flow")
+        headers = auth.get("headers", {})
+        print("Headers: ", headers)
+        data = {"grant_type": "client_credentials"}
+        client_id = auth.get("clientId")
+        client_secret = auth.get("clientSecret")
+        basic_auth = HTTPBasicAuth(client_id, client_secret)
+        print("Data: ", data)
+        token_resp = requests.post(
+            url=auth["tokenUrl"],
+            headers=headers,
+            auth=basic_auth,
+            data=data,
+            timeout=60,
+        )
+        token_resp.raise_for_status()
+        token = token_resp.json()["access_token"]
+        return {"Authorization": f"Bearer {token}"}, None
+
+    return {}, None
